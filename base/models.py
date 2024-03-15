bf0d28d5f9dca3d81ea8b243227a151e3441bb41
alejandroP03 <d.pulidob@uniandes.edu.co>
2024-03-15 15:27:52 -0500

    Created models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)    
+
+
+
+class UserCheckpoint(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    checkpoint_name = models.CharField(max_length=100)
+    checkpoint_url_endpoint = models.CharField(max_length=150)
+    creation_date = models.DateTimeField(auto_now_add=True)
